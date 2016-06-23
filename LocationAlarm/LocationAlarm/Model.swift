2ffb73a4728db12a5a5b6027e6a471c44c9fbff3
cardoso-ana <AnaCardoso@Anas-MacBook-Pro-2.local>
2016-06-23 20:21:14 -0300

    Notificação watch + constraints

--- LocationAlarm/LocationAlarm/Model.swift
+++ LocationAlarm/LocationAlarm/Model.swift
@@ -14,9 +14,6 @@ class Model: NSObject
     ///Audio player responsible for playing sound files.
     var audioPlayer: AVAudioPlayer = AVAudioPlayer()
     
-    ///User's bookmarked sound files.
-    var bookmarkedFiles: [String] = []
-    
     ///User defaults
     var userDefaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
     class func sharedInstance() -> Model
